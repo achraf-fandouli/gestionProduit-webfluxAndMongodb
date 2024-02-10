0bd64fa8041db65096af8b37003b3d47c60ee0e8
achraf-fandouli <achref.fendouli@gmail.com>
2024-02-11 00:54:38 +0100

    setup dependencies

---
+++ src/main/java/com/digitalcode/webflux/GestionProduitWebfluxAndMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.digitalcode.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionProduitWebfluxAndMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionProduitWebfluxAndMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digitalcode/webflux/GestionProduitWebfluxAndMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalcode.webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionProduitWebfluxAndMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
